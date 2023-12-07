28f4a931844ffc5ce573c6dc5b67c8ac668a5948
baptistecrts <baptiste.courties@viacesi.fr>
2023-12-07 23:20:57 +0100

    datagridview

---
+++ InterfaceClient.h
@@ -0,0 +1,248 @@
+﻿#pragma once
+#include <msclr/marshal_cppstd.h>
+#include "Manager.h"
+
+namespace ProjetPOO {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Description résumée de InterfaceClient
+	/// </summary>
+	public ref class InterfaceClient : public System::Windows::Forms::Form
+	{
+	public:
+		InterfaceClient(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ajoutez ici le code du constructeur
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		~InterfaceClient()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::DateTimePicker^ dateTimePicker1;
+	private: System::Windows::Forms::DateTimePicker^ dateTimePicker2;
+
+	private:
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->dateTimePicker1 = (gcnew System::Windows::Forms::DateTimePicker());
+			this->dateTimePicker2 = (gcnew System::Windows::Forms::DateTimePicker());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(123, 39);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(140, 20);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Ajouter un client";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(114, 96);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(39, 16);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Nom";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(93, 128);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(60, 16);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Prenom";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(7, 164);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(146, 16);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"Adresse Facturation";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(21, 202);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(132, 16);
+			this->label5->TabIndex = 4;
+			this->label5->Text = L"Adresse Livraison";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(38, 241);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(115, 16);
+			this->label6->TabIndex = 5;
+			this->label6->Text = L"Date naissance";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label7->Location = System::Drawing::Point(38, 280);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(115, 16);
+			this->label7->TabIndex = 6;
+			this->label7->Text = L"Date Inscription";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(150, 321);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(95, 34);
+			this->button1->TabIndex = 7;
+			this->button1->Text = L"Accepter";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &InterfaceClient::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(159, 95);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(200, 20);
+			this->textBox1->TabIndex = 8;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(159, 128);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(200, 20);
+			this->textBox2->TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(159, 163);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(200, 20);
+			this->textBox3->TabIndex = 10;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(159, 202);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(200, 20);
+			this->textBox4->TabIndex = 11;
+			// 
+			// dateTimePicker1
+			// 
+			this->dateTimePicker1->Location = System::Drawing::Point(159, 241);
+			this->dateTimePicker1->Name = L"dateTimePicker1";
+			this->dateTimePicker1->Size = System::Drawing::Size(200, 20);
+			this->dateTimePicker1->TabIndex = 12;
+			// 
+			// dateTimePicker2
+			// 
+			this->dateTimePicker2->Location = System::Drawing::Point(159, 280);
+			this->dateTimePicker2->Name = L"dateTimePicker2";
+			this->dateTimePicker2->Size = System::Drawing::Size(200, 20);
+			this->dateTimePicker2->TabIndex = 13;
+			// 
+			// InterfaceClient
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Silver;
+			this->ClientSize = System::Drawing::Size(397, 386);
+			this->Controls->Add(this->dateTimePicker2);
+			this->Controls->Add(this->dateTimePicker1);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"InterfaceClient";
+			this->Text = L"InterfaceClient";
+			this->Load += gcnew System::EventHandler(this, &InterfaceClient::InterfaceClient_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void InterfaceClient_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
+}
