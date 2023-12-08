4f9c0dfff06347c7a26e0d9cc1bc272172a84ad5
baptistecrts <baptiste.courties@viacesi.fr>
2023-12-08 02:34:02 +0100

    nom colone

--- Manager.h
+++ Manager.h
@@ -5,13 +5,6 @@
 
 
 enum collone;
-Array < String^> coloneArticle = {  "ID","Nom","Nature","Couleur","PrixHT","PrixTTC","Qte_stocker","seuil_reappro","taux_TVA","Prix_variable","Prix_fournisseur","en_vente" };
-
-Array < String^> colonePersonnel = {"ID","Nom","Prenom","Poste","Adresse""Date_embauche","ID_superieur" };
-
-Array < String^>coloneClient = { "ID","Nom","Prenom","Adresse_facturation","Adresse_livraison","Date_de_naissance","Premier_achat","client_inscrit" };
-
-Array < String^> coloneCommande = { "ID","Date_livraison","Date_paiement","Date_emission","Reference""Remise""Date_creation_commande","ID_client","ID_Personnel" };
 
 
  ref class  Manager
